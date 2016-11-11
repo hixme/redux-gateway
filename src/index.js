b145502a770560fd66d87585f856e9efd8cfcffd
Billy Montgomery <billy.montgomery@gmail.com>
2016-11-11 14:43:36 -0800

    UD-64: add response selector for requests

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reducer from './reducer'
 export { setGateway } from './gateway'
 export connectRequest from './connectRequest'
 export const getRequestByName = selectors.getRequestByName
+export const getResponseByRequestName = selectors.getResponseByRequestName
 export const createRequest = actions.createRequest
 export const clearRequest = actions.clearRequest
 
